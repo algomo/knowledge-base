fe3e041aba4febb63a30bf9e997375ed9df20d08
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-07-05 11:27:19 +0100

    navbar + footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,58 +1,119 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+
+/** @type {import('@docusaurus/plugin-content-docs').Options[]} */
+const docs = [
+  // {
+  //   id: "getting-started",
+  //   path: "docs/getting-started",
+  //   routeBasePath: "/getting-started",
+  // },
+  {
+    id: "widgets",
+    path: "docs/widgets",
+    routeBasePath: "/widgets",
+  },
+  {
+    id: "chatbots",
+    path: "docs/chatbots",
+    routeBasePath: "/chatbots",
+  },
+  {
+    id: "data-sources",
+    path: "docs/data-sources",
+    routeBasePath: "/data-sources",
+  },
+  {
+    id: "tools",
+    path: "docs/tools",
+    routeBasePath: "/tools",
+  },
+  {
+    id: "inbox",
+    path: "docs/inbox",
+    routeBasePath: "/inbox",
+  },
+];
+
+/** @type {import('@docusaurus/plugin-content-docs').Options} */
+const defaultSettings = {
+  breadcrumbs: false,
+  showLastUpdateTime: true,
+  // remarkPlugins: [
+  //   [require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }],
+  // ],
+  sidebarPath: require.resolve("./sidebars-default.js"),
+};
+
+/**
+ * Create a section
+ * @param {import('@docusaurus/plugin-content-docs').Options} options
+ */
+function create_doc_plugin({
+  sidebarPath = require.resolve("./sidebars-default.js"),
+  ...options
+}) {
+  return [
+    "@docusaurus/plugin-content-docs",
+    /** @type {import('@docusaurus/plugin-content-docs').Options} */
+    ({
+      ...defaultSettings,
+      sidebarPath,
+      ...options,
+    }),
+  ];
+}
+
+const docs_plugins = docs.map((doc) => create_doc_plugin(doc));
+
+const plugins = [...docs_plugins];
+
+/** @type {import('@docusaurus/types').Config} */
+const meta = {
+  title: "Knowledge Base",
+  tagline: "",
+  favicon: "img/favicon.png",
+  url: "https://algomo.com",
+  baseUrl: "/",
+  i18n: {
+    defaultLocale: "en",
+    locales: ["en"],
+  },
+};
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  ...meta,
+  plugins,
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+  trailingSlash: false,
+  themes: ["@docusaurus/theme-live-codeblock"],
+  onBrokenLinks: "ignore",
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          path: "docs/getting-started",
+          id: "getting-started",
+          routeBasePath: "/getting-started",
+          ...defaultSettings,
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
+        blog: false,
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+        sitemap: {
+          ignorePatterns: ["/tags/**"],
+        },
+        // TODO: Add your google analytics key
+        googleTagManager: {
+          containerId: "GTM-5FDFFSS",
         },
       }),
     ],
@@ -62,72 +123,150 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/favicon.png",
+      docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
       navbar: {
-        title: 'My Site',
+        title: "",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "Algomo Logo",
+          src: "img/logo-black.svg",
+          srcDark: "img/logo-white.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            to: "getting-started/",
+            position: "left",
+            label: "Getting Started",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            to: "widgets/",
+            position: "left",
+            label: "Widgets",
+          },
+          {
+            to: "chatbots/",
+            position: "left",
+            label: "Chatbots",
+          },
+          {
+            to: "data-sources/",
+            position: "left",
+            label: "Data Sources",
+          },
+          {
+            to: "tools/",
+            position: "left",
+            label: "Tools",
+          },
+          {
+            to: "inbox/",
+            position: "left",
+            label: "Inbox",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Getting Started",
+                to: "/getting-started",
+              },
+              {
+                label: "Widgets",
+                to: "/widgets",
+              },
+              {
+                label: "Chatbots",
+                to: "/chatbots",
+              },
+              {
+                label: "Data Sources",
+                to: "/data-sources",
+              },
+              {
+                label: "Tools",
+                to: "/tools",
+              },
+              {
+                label: "Inbox",
+                to: "/inbox",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Industries",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "E-Commerce",
+                href: "https://www.algomo.com/industries/ecommerce",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "SaaS",
+                href: "https://www.algomo.com/industries/saas",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "FinTech",
+                href: "https://www.algomo.com/industries/fintech",
+              },
+              {
+                label: "Travel",
+                href: "https://www.algomo.com/industries/travel",
+              },
+            ],
+          },
+
+          {
+            title: "Community",
+            items: [
+              {
+                label: "GitHub",
+                href: "https://github.com/algomo",
+              },
+              {
+                label: "Facebook",
+                href: "https://www.facebook.com/algomocom",
+              },
+              {
+                label: "Twitter",
+                href: "https://twitter.com/Algomocom",
+              },
+              {
+                label: "LinkedIn",
+                href: "https://www.linkedin.com/company/algomo/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "Check More",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Our Website",
+                href: "https://algomo.com",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "Privacy Policy",
+                href: "https://www.algomo.com/privacy-policy",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Algomo, Inc.`,
+        logo: {
+          alt: "Algomo Logo",
+          src: "img/logo-white.svg",
+          style: {
+            width: "200px",
+          },
+          href: "https://algomo.com",
+        },
       },
       prism: {
         theme: lightCodeTheme,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,24 @@
-import React from 'react';
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import HomepageFeatures from "@site/src/components/HomepageFeatures";
 
-import styles from './index.module.css';
+import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx("hero hero--primary", styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title">
+          Customer support, powered by generative AI
+        </h1>
+        <p className="hero__subtitle">
+          Automate and optimize your customer and employee support in 100+
+          languages, in less than 3 minutes.
+        </p>
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -27,7 +32,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
