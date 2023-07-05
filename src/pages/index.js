5e50ae334834a9a8b527f236ff48fdc4c2732caf
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-07-05 11:57:21 +0100

    hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,22 +8,28 @@ import HomepageFeatures from "@site/src/components/HomepageFeatures";
 import styles from "./index.module.css";
 
 function HomepageHeader() {
-  const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
+    <header className={styles.custom__bg}>
+      <div className="">
         <h1 className="hero__title">
-          Customer support, powered by generative AI
+          Customer support, <br /> powered by generative AI
         </h1>
         <p className="hero__subtitle">
           Automate and optimize your customer and employee support in 100+
           languages, in less than 3 minutes.
         </p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+          <Link className={styles.arrow_button_wrap} to="/getting-started">
+            <div class={styles.flexed_button_content}>
+              <div>Get Started</div>
+              <div class={styles.space_horizontal_tiny}></div>
+              <img
+                src="https://uploads-ssl.webflow.com/6421a177cdeeaf4e0591b744/6421a177cdeeaf275891b7b8_Arrow%20right%20long%20WHITE.svg"
+                loading="lazy"
+                alt=""
+                class={styles.image_arrow}
+              />
+            </div>
           </Link>
         </div>
       </div>
