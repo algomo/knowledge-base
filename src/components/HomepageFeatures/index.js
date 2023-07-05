3414d9f345bd8b465e82ea3d7d066bd98e5fd163
AhmedCoolProjects <bargadyahmed2000@gmail.com>
2023-07-05 12:39:18 +0100

    homepage v1

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -1,51 +1,63 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import Link from "@docusaurus/Link";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Widgets",
+    img: "img/widgets.png",
+    link: "/widgets",
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        The <code>UI element</code> where your visitors will interact with{" "}
+        <code>algomo</code> if you install it on a website.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Chatbots",
+    img: "img/chatbots.png",
+    link: "/chatbots",
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Our chatbots are <code> MULTILINGUAL NATIVELY</code>, i.e. you can
+        provide information in any language, and they can respond back to any
+        language.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Data Sources",
+    link: "data-sources",
+    img: "img/data-sources.png",
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Any collection of data that can be used to train our chatbots. We have a
+        growing list of data sources, and you can also provide your own from
+        different sources.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ img, title, description, link }) {
   return (
-    <div className={clsx('col col--4')}>
+    <Link
+      to={link}
+      style={{
+        textDecoration: "none",
+        color: "inherit",
+      }}
+      className={clsx("col col--4")}>
       <div className="text--center">
-        <Svg className={styles.featureSvg} role="img" />
+        <img className={styles.featureSvg} src={img} alt={title} />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
         <p>{description}</p>
       </div>
-    </div>
+    </Link>
   );
 }
 
@@ -53,10 +65,38 @@ export default function HomepageFeatures() {
   return (
     <section className={styles.features}>
       <div className="container">
+        <h1
+          style={{
+            textAlign: "center",
+            marginBottom: "2rem",
+          }}>
+          Features!
+        </h1>
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            padding: "2rem",
+          }}>
+          <Link className={styles.arrow_button_wrap} to="/getting-started">
+            <div class={styles.flexed_button_content}>
+              <div>Check more</div>
+              <div class={styles.space_horizontal_tiny}></div>
+              <img
+                src="https://uploads-ssl.webflow.com/6421a177cdeeaf4e0591b744/6421a177cdeeaf275891b7b8_Arrow%20right%20long%20WHITE.svg"
+                loading="lazy"
+                alt=""
+                class={styles.image_arrow}
+              />
+            </div>
+          </Link>
         </div>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -41,8 +41,8 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Learning resources to help you get started on Algomo.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
